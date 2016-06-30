5580a479b8ab5e58658b642b60bb3bfbd7617de0
Andrei Pozolotin <andrei.pozolotin@gmail.com>
2016-06-30 23:23:20 +0000

    A  .gitignore A  acbuild/APKBUILD A  acserver/APKBUILD A  alpiner-package.launch A  alpiner-package.launch.sh A  build.sh A  swt/APKBUILD A  swt/build.xml A  swt/reference/build.sh A  swt/reference/make_linux.mak A  swt/reference/readme.md

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e -x
+
+repo_get() {
+    if [[ -e "$repo_dir/.git" ]] ; then
+        cd "$repo_dir"
+        git reset --hard
+        git clean --force
+        git pull
+    else
+        rm -r -f "$repo_dir"
+        git clone "$repo_url" "$repo_dir"
+    fi
+}
+
+repo_put() {
+    cd "$repo_dir"
+    git add --all  :/
+    git status 
+    local message=$(git status --short)
+    git commit --message "$message"
+    git push
+}
+
+fetch_deps() {
+    local deps_list=$(source "$project/APKBUILD" && echo $makedepends)
+    sudo apk add $deps_list
+}
+
+copy_keys() {
+    local source="$HOME/.abuild"
+    local target="$repo_dir/keys"
+    mkdir -p "$target"
+    cp -f "$source"/*.pub "$target"
+}
+
+make_tags() {
+    local pkgname=$(source "$project/APKBUILD" && echo $pkgname)
+    local pkgver=$(source "$project/APKBUILD" && echo $pkgver)
+    local artifact="${pkgname}-${pkgver}"
+    git tag -a -f -m "$artifact" "$artifact"
+}
+
+build_apk() {
+    cd "$project"
+    abuild -P "$repo_dir" checksum
+    abuild -P "$repo_dir"
+}
+
+###
+
+readonly location=$(cd $(dirname $0) && pwd)
+readonly project="$location/$1"
+readonly repo_url="git@github.com:random-alpiner/repository.git"
+readonly repo_dir="$HOME/repository"
+
+repo_get
+fetch_deps
+make_tags
+build_apk
+copy_keys
+repo_put
---
+++ swt/reference/readme.md
@@ -0,0 +1,21 @@
+
+### swt sources
+
+https://www.eclipse.org/swt/git.php
+
+http://git.eclipse.org/c/platform/eclipse.platform.swt.git/
+
+http://git.eclipse.org/c/platform/eclipse.platform.swt.git/tree/bundles/org.eclipse.swt
+
+### swt native loader
+
+eclipse.platform.swt/bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+
+eclipse.platform.swt/bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+
+### core libunixfile
+
+[pid  9515] open("/home/work/space/none-4.5.0/conf/org.eclipse.osgi/97/0/.cp/os/linux/x86_64/libunixfile_1_0_0.so", O_RDONLY|O_NONBLOCK|O_CLOEXEC|O_PATH) = 115
+https://github.com/eclipse/eclipse.platform.resources
+git://git.eclipse.org/gitroot/platform/eclipse.platform.resources.git
+https://github.com/eclipse/eclipse.platform.resources/blob/master/bundles/org.eclipse.core.filesystem/src/org/eclipse/core/internal/filesystem/local/LocalFileNatives.java
